732310386ace84dc0ba0d9787013405dc0271270
Jesse Dijkman <jesse.dijkman@hva.nl>
2018-11-08 12:39:32 +0100

    Created the code for getting data from different years. And the code for getting a total amount of each genre per year

--- README.md
+++ README.md
@@ -11,6 +11,8 @@ For this project we're fetching data from the oba (public library of Amsterdam) 
   - [Picked Questions](#picked-questions)
   - [Sub-Questions](#sub-questions)
   - [Hypotheses](#hypotheses)
+  - [Researching the data](#researching-the-data)
+- [Sources](#sources)
 
 ## To-do 📋
 - [ ] Write down the research questions and pick the best ones 🤓
@@ -46,26 +48,62 @@ npm install
 - 👉**Are certain genres returned too late more often than others?**
 - 👉**Which genres have have emerged since 1950?**
 
-  #### Picked Questions 📍
+#### Picked Questions 📍
 - How long are books borrowed on average per year?
   - **v.2** How long are books from certain genres borrowed per year on average?
 - Are certain genres returned too late more often than others?
-- Which genres have have emerged since 1950?
+- 👉Which genres have have emerged since 1950?
   - **v.2** How has the popularity changed of certain genres since 1950?
   - **v.3** Which genres are there from the year 2000?
-### Sub-Questions 👶
-- Do certain changes in popularity of a genre have a connection with an event/happening from that year?
-- Can the popularity of genres be categorised by province?
-- Can the popularity of genres be categorised by gender?
+
+Which version I'm going to pick from the question: "Which genres have emerged since 1950?", depends on the data I have at the end. Now I can get v.3 done, but v.2 might be possible when I loop through different years.
+
+### Sub-Question(s) 👶
+- What has the publication year have to do with the most popular genre?
+
 
 ### Hypotheses 🤓
--
+- The publication year has nothing to do with the most popular genres.
+- Our interests have shifted over the years.
 
+### Researching the data
+To come up with research questions we had to research the data we we're served. The data comes to us in JSON, but is originally XML. Thanks to rijkvanzanten's package we can talk to the api and get JSON. From there we had to see what data we got.
+Analyzing the data I saw that a lot was missing, and trying to look certain things that aren't there gives errors. To prevent such errors I wrote some code that checks if there is a key, and check if it's an array. With this code I was able to push it into my own array in the format I wanted. The only problem is that I didn't need all that data.
 
-### What data do I need?
-- Genres => Name, amount
-- Publication year
-- (Location(s) of the books)
+[screenshot]
+
+The next step was to get multiple pages. Trying to get multiple pages I ran into "asynchronous problems". Where I tried to loop through the pages with a forEach loop. Getting different pages with data would work but they wouldn't be returned in the same order.
+
+[screenshot]
+
+With the previous step taken I had to make it so I would get the data in the same order each time. That's where async and await comes in. With async and await I forced my code to wait until all the data from a page is fetched and then go to the next page.
+
+[screenshot]
+
+Now that I was able to get data in the same order each time. I wanted to get ALL the data, which differs per search-query. First I needed to know how many results I could get. This information can be found in the meta from aquabrowser (JSON.parse(res).aquabrowser.meta.count).
+
+[screenshot]
+
+Now that I knew how much data there is, I can loop through the pages, right? NOPE. Looping through the pages worked but the api threw an error randomly, because they don't support that many requests. So I wrote some code that would restart the loop by looking at where it crashed. Finding the spot was easy, just getting the length from my array with data and dividing it by 20 would give me the page where I was. Then I just restarted the code but now with a different starting value for the loop.
+
+[screenshot]
+
+Now it works. But it's not fast. Now my goal was to get data from different years. But this proved to be trickier than expected. I couldn't wrap my head around it, plus I was running into errors all day long and just lost my focus. That's why I changed my research question to something simpler.
+
+**Question before:** _How has the popularity changed of certain genres since 1950?_
+**Question after:** _Which genres are there from the year 2000?_
+
+Focusing on just one year made it really easy. The big problem is that I needed to search for something, so I just searched for the letter 'a'. This would give me around 9000 books, so I knew that I needed a max amount.
+
+[screenshot]???
+
+Now that I was getting more specific. I realised that I need to specify my data to try and answer my research question. So I just commented-out most of the code, and just started to gather the genres. And from there I wroted code that would count how many times a certain genre would come up. This data is pushed to the array, and would stop pushing to the array when I had a totalAmt of at least 100.
+
+With this data I tried to make a first visualization on Observable (link). Most of the code was already there so I just needed to replace the data. And it worked. But that was too easy, because that would mean I was almost done. So now I wanted to (re)try and get data from different years.
 
 
 ## Code 🤓🤓🤓
+
+
+## Source(s)
+- https://github.com/rijkvanzanten/node-oba-api
--- index.js
+++ index.js
@@ -9,14 +9,14 @@ const client = new OBA({
   secret: process.env.SECRET
 });
 
-const maxData = 100;
-const publicationYear = 2000;
-var query = 'oorlog'
-var genresStore = [];
+const maxData = 50;
+let publicationYear = 2000;
+let yearRange = 2002;
 var save = [];
-var z = 1;
 
+var query = 'oorlog'
 
+var rememberPage = 0;
 
 function getTotalBooks() {
   return client.get('search', {
@@ -24,16 +24,16 @@ function getTotalBooks() {
     sort: 'title',
     librarian: 'true',
     refine: 'true',
-    facet: `pubYear(${publicationYear})`,
+    facet: `pubYear(${publicationYear})`
   })
   .then(res => {
-    console.log(JSON.parse(res).aquabrowser.meta)
     return Math.ceil(JSON.parse(res).aquabrowser.meta.count / 20);
   })
-  .catch(error => console.log(error))
+  .catch(error => console.log(error, 'oi'))
 }
 
 function getData(i) {
+
   return new Promise((resolve, reject) => {
     client.get('search', {
       q: query,
@@ -46,134 +46,100 @@ function getData(i) {
       .then(res => {
         var correctRes = JSON.parse(res).aquabrowser.results.result;
         const error = false;
-
+        // Temp is just to keep the different years separated
+        var temp = [];
         correctRes.forEach((book, ind, page) => {
           if (book.genres) {
-            if (book.genres.genre.length > 1) {
-              book.genres.genre.forEach(g => {
-                if (!genresStore.includes(g.$t)) {
-                  genresStore.push(g.$t);
-                  save.push({
-                    genre: g.$t,
-                    amt: 1
+            if (Array.isArray(book.genres.genre)) {
+              // Now loop through the genres of book
+              book.genres.genre.forEach(genreInArray => {
+                let search = temp.find(obj => obj.genre === genreInArray.$t);
+                if (!search) {
+                  // Not found? Push to temp
+                  temp.push({
+                    genre: genreInArray.$t,
+                    amt: 1,
+                    date: `${publicationYear}`
                   })
                 } else {
-                  save.forEach((obj, index, arr) => {
-                    if (g.$t == obj.genre) {
-                      obj.amt++
-                    }
-                  })
+                  // Found it? Add 1 to amt
+                  search.amt++
                 }
               })
             } else {
-              if (!genresStore.includes(book.genres.genre.$t)) {
-                genresStore.push(book.genres.genre.$t);
-                save.push({
+              // book.genres.genre is a single genre
+              let search = temp.find(obj => obj.genre === book.genres.genre.$t);
+              if (!search) {
+                temp.push({
                   genre: book.genres.genre.$t,
-                  amt: 1
+                  amt: 1,
+                  date: `${publicationYear}`
                 })
               } else {
-                save.forEach((obj, index, arr) => {
-                  if (book.genres.genre.$t == obj.genre) {
-                    obj.amt++
-                  }
-                })
+                search.amt++
               }
             }
           }
-
-        if (ind == page.length - 1) {
-            if (!error) {
-              resolve()
-            } else {
-              reject('error 2')
-            }
-          }
         })
-
-        // ==============================
-
-          // var structuredData = () => correctRes.map(x => {
-          //
-          //   return {
-          //     title: (x.titles) ? (x.titles.title) ? (x.titles.title.length > 1) ? x.titles.title.shift().$t : x.titles.title.$t : undefined : undefined,
-          //     format: (x.formats) ? (x.formats.format) ? (x.formats.format.length > 1) ? x.formats.format.pop().$t : x.formats.format.$t : undefined : undefined,
-          //     author: (x.authors) ? (x.authors['main-author']) ? x.authors['main-author'].$t : undefined : undefined,
-          //     publication: (x.publication) ? (x.publication.year) ? x.publication.year.$t : undefined : undefined,
-          //     physicalInfo: (x.description) ? (x.description['physical-description']) ? x.description['physical-description'].$t : undefined : undefined,
-          //     language: (x.languages) ? (x.languages.language) ? x.languages.language.$t : undefined : undefined,
-          //     genre: (x.genres) ? (x.genres.genre.length > 1) ? x.genres.genre.map(g => g.$t) : x.genres.genre.$t : undefined,
-          //     subject: (x.subjects) ? (x.subjects['topical-subject']) ? (x.subjects['topical-subject'].length > 1) ? x.subjects['topical-subject'].map(s => s.$t) : x.subjects['topical-subject'].$t : undefined : undefined,
-          //     publisher: {
-          //       name: (x.publication) ? (x.publication.publishers) ? x.publication.publishers.publisher.$t : undefined: undefined,
-          //       place: (x.publication) ? (x.publication.publishers) ? x.publication.publishers.publisher.place : undefined : undefined
-          //     }
-          // }
-          //
-          //
-          // })
-          // if (!error) {
-          //   resolve(structuredData())
-          // } else {
-          //   reject('error 2')
-          // }
-        })
-
-      .catch(err => {
-        z = temp.length / 20;
-        updatePage()
+        if (!error) {
+          // Send temp back for concat with save
+          resolve(temp)
+        } else {
+          reject('Error in correctRes.forEach')
+        }
       })
+      .catch(err => {
+        console.log(err)
+
+      })
+
   })
 }
 
 async function updatePage() {
+  // console.log(publicationYear)
   var totalData = await getTotalBooks();
-  var totalAmt;
-  for (z; z <= totalData; z++) {
-    console.log(totalAmt, save)
-
-    // Set a max and stop the loop when reached
-    if (totalAmt > maxData) {
-      break;
+  let temp = [];
+  let totalAmt;
+  for (let z = 1; z <= totalData; z++) {
+    if (temp.length) {
+      totalAmt = temp.map(t => t.amt).reduce((a, c) => a + c);
     }
-    await getData(z)
-      // .then(res => {
-        // console.log(res)
-        // console.log(save)
-        // res.forEach(x => {
-          // save.push(x)
-          // if (x.genre) {
-        //   console.log(`
-        //     Title: ${x.title}
-        //     Format: ${x.format}
-        //     Author: ${x.author}
-        //     Publication: ${x.publication}
-        //     Physical information: ${x.physicalInfo}
-        //     Language: ${x.language}
-        //     Genre(s): ${x.genre}
-        //     Subject(s): ${x.subject}
-        //     Publisher name: ${x.publisher.name} | Publisher place: ${x.publisher.place}
-        //     `)
-        // }
-      // })
-      // console.log(save.length)
-    // })
-      // .catch(error => console.log(error))
+    var con = await getData(z).then(res => {
+      // Add res to final data save (save)
+      if (!temp.length) {
+        temp = temp.concat(res);
+      } else if (totalAmt <= maxData) {
 
-    totalAmt = save.map(x => {
-      return x.amt
-    }).reduce((a, b) => {
-      return a + b
-    })
-    // console.log(totalAmt, save)
-    // if (totalAmt == 100) {
-    //   break;
-    // }
+        // console.log(totalAmt)
+
+        res.forEach(r => {
+          let search = temp.find(t => t.genre === r.genre);
+          if (!search) {
+            temp.push(r)
+          } else {
+            search.amt += r.amt;
+          }
+        })
+      } else {
+        save = save.concat(temp);
+        return true
+
+      }
+      }).catch(err => console.log(err))
+      if (con) {
+
+        if (publicationYear < yearRange) {
+          publicationYear++
+
+          updatePage()
+          break;
+        } else {
+          console.log(save)
+          break;
+        }
+      }
   }
-  // Get the total amt of genres
-
-
 }
-
 
 updatePage()
